4c278c76e6e66490130ce330bfccb4fa7ca4941d
J4vakhishvili <71881956+j4vakhishvili@users.noreply.github.com>
2022-03-15 15:56:24 +0400

    yes

---
+++ assets/script/buttons.js
@@ -0,0 +1,9 @@
+function oNav(){
+    document.getElementById("nav").style.height = "100%";
+    document.getElementsByClassName("nav").style.height = "100%";
+}
+
+function cNav(){
+    // document.getElementById("nav").style.height = "0%";
+    document.getElementsByClassName(".nav").style.height = "100%";
+}
