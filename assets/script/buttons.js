745724e2b417345950d283b11f3ab57c1b6829a3
J4vakhishvili <71881956+j4vakhishvili@users.noreply.github.com>
2022-05-24 22:39:24 +0400

    welcome to the new devi

--- assets/script/buttons.js
+++
@@ -1,9 +0,0 @@
-function oNav(){
-    document.getElementById("nav").style.height = "100%";
-    document.getElementsByClassName("nav").style.height = "100%";
-}
-
-function cNav(){
-    // document.getElementById("nav").style.height = "0%";
-    document.getElementsByClassName(".nav").style.height = "100%";
-}
