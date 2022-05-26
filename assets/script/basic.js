8f4b6b09c7de795f2ead51ab8337f4bcc55c1ec1
J4vakhishvili <71881956+j4vakhishvili@users.noreply.github.com>
2022-05-26 22:24:56 +0400

    responsive

--- assets/script/basic.js
+++ assets/script/basic.js
@@ -1,3 +1,16 @@
 const date = new Date();
 let year = date.getFullYear();
 document.getElementById("date").innerHTML = "2021 - " + year;
+
+//openNav
+function openNav() {
+    document.getElementById("nav").style.top = "0";
+    document.getElementById("openNav").style.display = "none";
+    document.getElementById("closeNav").style.display = "block";
+}
+  
+function closeNav() {
+    document.getElementById("nav").style.top = "-96%";
+    document.getElementById("openNav").style.display = "block";
+    document.getElementById("closeNav").style.display = "none";
+}
