7f008e5ae188fa2e4629836e02c5b5ecffa7a89b
J4vakhishvili <71881956+j4vakhishvili@users.noreply.github.com>
2022-06-18 09:37:18 +0400

    Responsive menu

---
+++ assets/script/index-menu.js
@@ -0,0 +1,31 @@
+function openMenu() {
+    document.getElementById("menu").style.top = "0";
+}
+  
+function closeMenu() {
+    document.getElementById("menu").style.top = "-100%";
+}
+
+function openlessonsSection() {
+    document.getElementById("lessonsSection").style.display = "flex";
+    document.getElementById("btnLesson").style.display = "none";
+    document.getElementById("btnLessons").style.display = "flex";
+}
+  
+function closelessonsSection() {
+    document.getElementById("lessonsSection").style.display = "none";
+    document.getElementById("btnLesson").style.display = "flex";
+    document.getElementById("btnLessons").style.display = "none";
+}
+
+function opengakvetilebisSeqcia() {
+    document.getElementById("gakvetilebisSeqcia").style.display = "flex";
+    document.getElementById("btnGakvetili").style.display = "none";
+    document.getElementById("btnGakvetilebi").style.display = "flex";
+}
+  
+function closegakvetilebisSeqcia() {
+    document.getElementById("gakvetilebisSeqcia").style.display = "none";
+    document.getElementById("btnGakvetili").style.display = "flex";
+    document.getElementById("btnGakvetilebi").style.display = "none";
+}
